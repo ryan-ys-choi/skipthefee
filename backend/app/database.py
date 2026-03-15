0906b7129e6e14a63fe8b11b69e6f101d4e9e061
ryan-ys-choi <104918388+ryan-ys-choi@users.noreply.github.com>
2026-03-14 17:31:50 -0700

    Add search endpoint connecting FastAPI to PostgreSQL

---
+++ backend/app/database.py
@@ -0,0 +1,13 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        database="skipthefee",
+        user="postgres",
+        password=os.getenv("DB_PASSWORD")
+    )
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.database import get_connection
+
 
 app = FastAPI(title="SkipTheFee API")
 
@@ -18,3 +20,43 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+
+@app.get("/search")
+def search(restaurant: str, city: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+                   SELECT
+                    r.name,
+                    r.city,
+                    p.platform,
+                    p.item_name,
+                    p.item_price,
+                    p.delivery_fee,
+                    p.service_fee,
+                    p.total
+                   FROM restaurants r
+                    JOIN prices p ON r.id = p.restaurant_id
+                    WHERE LOWER(r.name) LIKE LOWER(%s)
+                    AND LOWER(r.city) = LOWER(%s)
+                    ORDER BY p.total ASC
+                   """, (f"%{restaurant}%", city))
+    
+    rows = cursor.fetchall()
+    cursor.close()
+    
+    results = []
+    for row in rows:
+        results.append({
+            "restaurant": row[0],
+            "city": row[1],
+            "platform": row[2],
+            "item_name": row[3],
+            "item_price": float(row[4]),
+            "delivery_fee": float(row[5]),
+            "service_fee": float(row[6]),
+            "total": float(row[7])
+        })
+
+    return {"results": results} 
